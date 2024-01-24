c0cdcb94930a67228afe3e0190dce1f8df58e4ce
FhRh <yiyop@naver.com>
2024-01-24 16:35:54 +0900

    add/entity, repository, dependencies - jpa의존성 추가 - repository 추가

--- user-service/src/main/java/com/example/userservice/service/UserServiceImpl.java
+++ user-service/src/main/java/com/example/userservice/service/UserServiceImpl.java
@@ -1,14 +1,20 @@
 package com.example.userservice.service;
 
+import com.example.userservice.jpa.UserEntity;
+import com.example.userservice.jpa.UserRepository;
 import com.example.userservice.vo.UserDto;
 import org.modelmapper.ModelMapper;
 import org.modelmapper.convention.MatchingStrategies;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.UUID;
 
 @Service
 public class UserServiceImpl implements UserService{
+    @Autowired
+    UserRepository userRepository;
+
     @Override
     public UserDto createUser(UserDto userDto) {
         userDto.setUserId(UUID.randomUUID().toString());
@@ -16,7 +22,7 @@ public class UserServiceImpl implements UserService{
         ModelMapper mapper = new ModelMapper();
         mapper.getConfiguration().setMatchingStrategy(MatchingStrategies.STRICT);
         UserEntity userEntity = mapper.map(userDto, UserEntity.class);
-        userEntity.setEncryptedPwd(passwordEncoder.encode(userDto.getPwd()));
+        userEntity.setEncryptedPwd("encrypted password");
 
         userRepository.save(userEntity);
 
